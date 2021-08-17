e8a795363271d0f0eec9d1429eaf8f8022b2968b
Ernesto Valecillos <evalecillos@gmail.com>
2021-08-16 21:16:21 -0500

    added the first robot glad related lines of code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert. JS is running!");
+var playerName = window.prompt("Hello! What is the name of your robot?");
+// note the lack of quoatation marks around playerName
+console.log(playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
